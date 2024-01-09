f9b70737027d0b843284cc21a4c16778c1eac181
Sammy Fischer <sammy@cosmic-bandito.com>
2024-01-09 12:22:11 +0100

    Add new model configs, cleanup script, and persona profile
    Added several new model configuration files, created a cleanup shell script for relocating obsolete models, and introduced a new persona profile. These changes bring more variety and flexibility to the

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+mv models/LoneStriker_dolphin-2.2-yi-34b-200k-4.0bpw-h6-exl2 obsolete_models/LoneStriker_dolphin-2.2-yi-34b-200k-4.0bpw-h6-exl2
+mv models/TheBloke_X-NoroChronos-13B-GPTQ obsolete_models/TheBloke_X-NoroChronos-13B-GPTQ
+mv models/TheBloke_Guanaco-13B-Uncensored-GPTQ obsolete_models/TheBloke_Guanaco-13B-Uncensored-GPTQ
+mv models/TheBloke_neural-chat-7B-v3-1-GPTQ obsolete_models/TheBloke_neural-chat-7B-v3-1-GPTQ
+mv models/TheBloke_TimeCrystal-L2-13B-GPTQ obsolete_models/TheBloke_TimeCrystal-L2-13B-GPTQ
+mv models/TheBloke_Spicyboros-7B-2.2-GPTQ obsolete_models/TheBloke_Spicyboros-7B-2.2-GPTQ
+mv models/DRAFT obsolete_models/DRAFT
+mv models/TheBloke_Hermes-LLongMA-2-13B-8K-GPTQ obsolete_models/TheBloke_Hermes-LLongMA-2-13B-8K-GPTQ
+mv models/TheBloke_MythoLogic-Mini-7B-GPTQ obsolete_models/TheBloke_MythoLogic-Mini-7B-GPTQ
+mv models/TheBloke_Wizard-Vicuna-13B-Uncensored-GPTQ obsolete_models/TheBloke_Wizard-Vicuna-13B-Uncensored-GPTQ
+mv models/TheBloke_AlpacaCielo2-7B-8K-GPTQ obsolete_models/TheBloke_AlpacaCielo2-7B-8K-GPTQ
+mv models/TheBloke_Nous-Hermes-Llama2-GPTQ_gptq-4bit-32g-actorder_True obsolete_models/TheBloke_Nous-Hermes-Llama2-GPTQ_gptq-4bit-32g-actorder_True
+mv models/LoneStriker_SUS-Chat-34B-3.0bpw-h6-exl2 obsolete_models/LoneStriker_SUS-Chat-34B-3.0bpw-h6-exl2
+mv models/LoneStriker_Nous-Capybara-limarpv3-34B-3.0bpw-h6-exl2-2 obsolete_models/LoneStriker_Nous-Capybara-limarpv3-34B-3.0bpw-h6-exl2-2
